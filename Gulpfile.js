cc1746d0084838d2f55f60f22dc53366ce1c0a0f
Adam Hoddinott <adam.hoddinott@salespreso.com>
2016-02-29 08:38:36 +1100

    feat(gulp): generate builds during dev

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,6 +6,7 @@ var watchify = require('watchify');
 
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var merge = require('merge-stream');
 
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
@@ -21,32 +22,34 @@ var DIST = "dist/";
 var BUILD = "build/";
 var DEMO = "demo/";
 
-var opts = {
-  entries: DEMO + 'index.js',
-  standalone: 'Three2D',
-  debug: true
-};
-
-var b;
-
 var pjson = require('./package.json');
 
-function bundle() {
-  return b.bundle()
-    // .pipe(source(pjson.name + '.js'))
-    .pipe(source('index.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(derequire())
-  // .pipe(uglify())
-    .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(DIST));
-}
-
 gulp.task('js', function() {
-  b = browserify(opts);
-  return bundle();
+  var build = browserify(assign({}, {
+    entries: SRC + pjson.name + '.js',
+    standalone: 'Three2D',
+    debug: true
+  }));
+
+  function bundle(name) {
+    return build.bundle()
+      .pipe(source(name))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(derequire())
+      .on('error', gutil.log);
+  }
+
+  var development = bundle(pjson.name + '.js')
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(BUILD));
+
+  var production = bundle(pjson.name + '.min.js')
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(BUILD));
+
+  return merge(development, production);
 });
 
 gulp.task('html', function() {
@@ -59,19 +62,37 @@ gulp.task('html-watch', ['html'], function() {
   return browserSync.reload();
 });
 
-gulp.task('watch', ['html'], function () {
+gulp.task('watch', ['html', 'js'], function () {
   browserSync.init({
     server: "./" + DIST,
     open: false
   });
 
-  b = watchify(browserify(assign({}, watchify.args, opts)));
-  b.on('update', function() {
-    bundle().on("end", browserSync.reload);
+  var demo = watchify(browserify(assign({}, watchify.args, {
+    entries: DEMO + 'index.js',
+    // standalone: 'Three2D',
+    debug: true
+  })));
+
+  function bundle() {
+    return demo.bundle()
+      .pipe(source('index.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(derequire())
+      .on('error', gutil.log)
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest(DIST));
+  }
+
+  demo.on('update', function() {
+    bundle().on('end', browserSync.reload);
   });
-  b.on('log', gutil.log);
+
+  demo.on('log', gutil.log);
 
   gulp.watch(DEMO + '*.html', ['html-watch'])
+  gulp.watch(SRC + '**/*.js', ['js'])
 
   return bundle()
     .pipe(browserSync.stream({ once: true }));
--- build/three-2d.js
+++ build/three-2d.js
@@ -1,22 +1,24 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Three2D = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 "use strict";
 
-// Export our es6 module as just the default function
-var Three2D = _dereq_("./three-2d").default;
-
-module.exports = Three2D;
-
-},{"./three-2d":2}],2:[function(_dereq_,module,exports){
-"use strict";
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-function Three2D() {}
-
 exports.default = Three2D;
+// Library code goes here (can be es6)
+function Three2D() {
+  return "Hello world";
+}
 
-},{}]},{},[1])(1)
+},{}],2:[function(_dereq_,module,exports){
+"use strict";
+
+// Export our es6 module as just the default function
+var Three2D = _dereq_("./main").default;
+
+module.exports = Three2D;
+
+},{"./main":1}]},{},[2])(undefined)
 });
 
 
